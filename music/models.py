d78221b86682e6d39ffd161d6da5e0dcb6e5ffe5
Sanu Ann Abraham <abraham.sanuann@gmail.com>
2017-08-23 19:01:23 -0400

    first commit

---
+++ music/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Album(models.Model):
+    artist = models.CharField(max_length=150)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE) #deleted when the corresponding album is deleted
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=200)
+
